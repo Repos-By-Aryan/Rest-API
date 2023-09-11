d5472377fcb383d9556290e6eeae5fdbcc16c602
Aryan <aryankumawat01@gmail.com>
2023-09-11 22:53:31 +0530

    get future and PostsModel

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:rest_api/models//PostsModel.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,6 +12,21 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  List<PostsModel> postList = [];
+
+  Future<List<PostsModel>> getPostApi()async{
+    final response =await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+    var data = jsonDecode(response.body.toString());
+    if( response.statusCode == 200){
+      for(Map i in data){
+        postList.add(PostsModel.fromJson(i));
+      }
+      return postList;
+    }else{
+      return postList;
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
